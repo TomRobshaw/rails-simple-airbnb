86b36ea484afacaaae769f172cce2f22fee9741f
Tom Robshaw <t.robshaw@outlook.com>
2022-11-15 16:34:05 +1100

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Jeffrey Dahmers spare bedroom',
+  address: '123 Geoff St',
+  description: 'A beautiful room full of dead people.',
+  price_per_night: 25,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Dark and dinghy',
+  address: 'skeleton creek, hauntsville',
+  description: 'A horrible 1 bed studio flat. 1 single bedroom, open plan living area, small kitchen and a disgusting balcony',
+  price_per_night: 15,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Kings Landing',
+  address: '10 Westeros Lane',
+  description: 'Youve just gotta see it to believe it',
+  price_per_night: 5575,
+  number_of_guests: 10
+)
